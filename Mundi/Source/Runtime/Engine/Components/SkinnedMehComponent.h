f277a15df0532e02ce9b84201412112afc16573d
WH <121390020+Jeongwoohyeong@users.noreply.github.com>
2025-11-08 03:55:28 +0900

    wif : skinning 코드 작성

---
+++ Mundi/Source/Runtime/Engine/Components/SkinnedMehComponent.h
@@ -0,0 +1,59 @@
+﻿#pragma once
+#include "MeshComponent.h"
+
+/*
+ *  TODO
+ *  dup, serialize
+ *  관절 공간 -> 모델 공간
+ *  skinning matrix
+ *  bone transform
+ *  
+ */
+class USkeletalMesh;
+
+class USkinnedMeshComponent : public UMeshComponent
+{
+public:
+    
+    DECLARE_CLASS(USkinnedMeshComponent, UMeshComponent)
+    GENERATED_REFLECTION_BODY()
+
+    USkinnedMeshComponent();
+    ~USkinnedMeshComponent() override;
+
+    void Serialize(const bool bInIsLoading, JSON& InOutHandle) override;
+    void TickComponent(float DeltaTime) override;
+    void CollectMeshBatches(TArray<FMeshBatchElement>& OutMeshBatchElements, const FSceneView* View) override;
+
+    void DuplicateSubObjects() override;
+    DECLARE_DUPLICATE(USkinnedMeshComponent)
+
+    void OnSerialized() override;    
+
+    void SetskeletalMesh(USkeletalMesh* InSkeletalMesh);
+    USkeletalMesh* GetSkeletalMesh() const { return SkeletalMesh; }
+
+protected:
+
+    void UpdateBoneMatrices();
+    void UpdateSkinningMatrices();
+    void PerfromCPUSkinning();
+
+private:
+    USkeletalMesh* SkeletalMesh = nullptr;
+
+    // 부모 뼈 기준의 자식 배치, 부모 기준 상대적 로컬 변환
+    // fbx에서 불러오거나 애니메이션에서 가져온다    
+    TArray<FMatrix> BoneSpaceTransforms;
+
+    // 애니메이션이 없을 때 Bind Pose, 존재하면 새로 계산해야함
+    // 특정 뼈의 로컬 -> 모델 공간
+    TArray<FMatrix> ComponentSpaceTransforms = {};
+
+    // Skinning Matrix
+    // 현재 애니메이션이 없어서 Inverse Bind Pose * Bind Pose
+    // 애니메이션 생기면 Inverse Bind Pose * BoneMatrix(Transformed Bind Pose) 
+    TArray<FMatrix> SkinningMatrix = {};
+
+    bool bChangedSkeletalMesh = false;
+};
---
+++ Mundi/Source/Runtime/Engine/Components/SkinnedMeshComponent.cpp
@@ -0,0 +1,140 @@
+﻿#include "pch.h"
+#include "SkinnedMehComponent.h"
+
+IMPLEMENT_CLASS(USkinnedMeshComponent)
+BEGIN_PROPERTIES(USkinnedMeshComponent)
+
+END_PROPERTIES(USkinnedMeshComponent)
+
+USkinnedMeshComponent::USkinnedMeshComponent()
+{
+    
+}
+
+USkinnedMeshComponent::~USkinnedMeshComponent()
+{
+    
+}
+
+void USkinnedMeshComponent::Serialize(const bool bInIsLoading, JSON& InOutHandle)
+{
+    UMeshComponent::Serialize(bInIsLoading, InOutHandle);
+}
+
+void USkinnedMeshComponent::TickComponent(float DeltaTime)
+{
+    UMeshComponent::TickComponent(DeltaTime);
+
+    if (!SkeletalMesh)
+    {
+        ComponentSpaceTransforms.Empty();
+        SkinningMatrix.Empty();
+        return;
+    }
+    
+    UpdateBoneMatrices();
+    UpdateSkinningMatrices();
+    
+}
+
+void USkinnedMeshComponent::CollectMeshBatches(TArray<FMeshBatchElement>& OutMeshBatchElements, const FSceneView* View)
+{
+    UMeshComponent::CollectMeshBatches(OutMeshBatchElements, View);
+}
+
+void USkinnedMeshComponent::DuplicateSubObjects()
+{
+    UMeshComponent::DuplicateSubObjects();
+}
+
+void USkinnedMeshComponent::OnSerialized()
+{
+    UMeshComponent::OnSerialized();
+}
+
+void USkinnedMeshComponent::SetskeletalMesh(USkeletalMesh* InSkeletalMesh)
+{
+    if(!InSkeletalMesh)
+    {
+        UE_LOG("[USkinnedMeshComponent/SetskeletalMesh] InSkeletalMesh is null");
+        return;
+    }
+    
+    if (InSkeletalMesh != SkeletalMesh)
+    {
+        SkeletalMesh = InSkeletalMesh;
+        bChangedSkeletalMesh = true;
+        UpdateBoneMatrices();
+        UpdateSkinningMatrices();
+    }    
+}
+
+void USkinnedMeshComponent::UpdateBoneMatrices()
+{    
+    FSkeletalMesh* MeshAsset = SkeletalMesh ? SkeletalMesh->GetSkeletalMeshAsset() : nullptr;
+    if (!MeshAsset)
+    {
+        UE_LOG("[USkinnedMeshComponent/UpdateBoneMatrices] FSkeletalMesh is null");
+        return;
+    }
+
+    const int32 BoneCount = MeshAsset->Bones.Num();
+    if (BoneCount == 0)
+    {
+        UE_LOG("[USkinnedMeshComponent/UpdateBoneMatrices] BoneCount is zero");
+        ComponentSpaceTransforms.Empty();
+        return;
+    }
+
+    // 지금은 애니메이션이 없어서 BindPose 그대로 사용
+    ComponentSpaceTransforms.SetNum(BoneCount);
+    for (int i = 0; i < BoneCount; i++)
+    {
+        // 애니메이션 없으니까 바인드 포즈 사용        
+        ComponentSpaceTransforms[i] = MeshAsset->Bones[i].GlobalTransform;
+    }
+
+    // TODO 애니메이션 구현 후 사용 BoneSpaceTransforms채워서 사용
+    // for (int i = 0; i < BoneCount; i++)
+    // {
+    //     FMatrix BoneLocal = BoneSpaceTransforms[i];
+    //     int32 ParentIndex = MeshAsset->Bones[i].ParentIndex;
+    //     if (ParentIndex == -1)
+    //     {
+    //         ComponentSpaceTransforms[i] = BoneLocal;
+    //     }
+    //     else
+    //     {
+    //         ComponentSpaceTransforms[i] = BoneLocal * ComponentSpaceTransforms[ParentIndex];
+    //     }
+    // }
+}
+
+void USkinnedMeshComponent::UpdateSkinningMatrices()
+{
+    // 본 매트릭스가 없으면 스키닝 안함
+    const int32 BoneCount = ComponentSpaceTransforms.Num();
+    if (BoneCount == 0)
+    {
+        UE_LOG("[USkinnedMeshComponent/UpdateSkinningMatrices] BoneMatrices is zero");
+        return;
+    }
+    
+    FSkeletalMesh* MeshAsset = SkeletalMesh ? SkeletalMesh->GetSkeletalMeshAsset() : nullptr;
+    if (!MeshAsset)
+    {
+        UE_LOG("[USkinnedMeshComponent/UpdateSkinningMatrices] FSkeletalMesh is null");
+        return;
+    }
+
+    SkinningMatrix.SetNum(BoneCount);
+    for (int i = 0; i < BoneCount; i++)
+    {
+        SkinningMatrix[i] = MeshAsset->Bones[i].InverseBindPoseMatrix * ComponentSpaceTransforms[i];
+    }
+}
+
+
+void USkinnedMeshComponent::PerfromCPUSkinning()
+{
+}
